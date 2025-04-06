11f1946b94d36f9c41e4dced4e6f25a9b45b6dd5
elafite <elafite@gmail.com>
2025-04-06 19:23:48 +0200

    misc

--- itunes-export.py
+++ itunes-export.py
@@ -6,18 +6,22 @@ import argparse
 
 parser = argparse.ArgumentParser(description="An utility application to export iTunes playlists in m3u format.")
 parser.add_argument("--output", "-o", help="The outpout folder for exporting the playlists.", required=True)
+parser.add_argument("--music_folder", "-m", help="The folder where the music files are stored", required=True)
 parser.add_argument("--ignore", help="Ignore a specific playlist.", action='append')
 parser.add_argument("--library", "-l", help="The path to the iTunes Library XML.", default=str(Path.home().joinpath("Music/iTunes/iTunes Music Library.xml")))
 parser.add_argument("--export-genius-playlists", action='store_true', dest='exportGeniusPlaylists')
 parser.add_argument("--export-smart-playlists", action='store_true', dest='exportSmartPlaylists')
+parser.add_argument("--absolute", action='store_true', dest='absolute')
+#parser.set_defaults(absolute=False)
 args = parser.parse_args()
 
 libraryPath = args.library
 playlistRootPath = Path(args.output)
+musicFolder = args.music_folder
 ignoreList= args.ignore if args.ignore is not None else []
 
 def cleanupPlaylistName(playlistName):
-        return playlistName.replace("/", "").replace("\\", "").replace(":", "")
+        return playlistName.replace("/", "").replace("\\", "").replace(":", "").replace("\"", "'").replace("?", "")
 
 def exportPlaylist(playlist: Playlist, parentPath: Path):
         if(playlist.is_genius_playlist and not args.exportGeniusPlaylists):
@@ -25,10 +29,10 @@ def exportPlaylist(playlist: Playlist, parentPath: Path):
 
         if(playlist.is_smart_playlist and not args.exportSmartPlaylists):
                 return
-
+        
         if(playlist.is_folder):
                 # Create Folder
-                currentPath = parentPath.joinpath(playlist.name)
+                currentPath = parentPath.joinpath(cleanupPlaylistName(playlist.name))
                 if(not currentPath.exists()):
                         currentPath.mkdir()
 
@@ -39,11 +43,18 @@ def exportPlaylist(playlist: Playlist, parentPath: Path):
                 playlistContent = ""
                 for track in playlist.tracks:
                         if track.location != None:
-                                try:
-                                        playlistContent +=  os.path.relpath(track.location, start=parentPath) + "\n"
-                                except ValueError:
-                                        print("Warning: Could not add the track \"" + track.location + "\" as relative path to the playlist \"" + playlistName + "\"; added the track as absolute path instead.")
-                                        playlistContent += track.location + "\n"
+                                if args.absolute is True:
+                                       playlistContent += track.location.replace("P:/Musique/iTunes", musicFolder) + "\n"
+                                else:
+                                        try:
+                                                #playlistContent +=  os.path.relpath(track.location, start=parentPath) + "\n"
+                                                #playlistContent +=  os.path.relpath(track.location.replace("P:/Musique/iTunes/iTunes Media", musicFolder), start=parentPath) + "\n"
+                                                playlistContent +=  os.path.relpath(track.location.replace("P:/Musique/iTunes", musicFolder), start=parentPath) + "\n"
+                                        except ValueError:
+                                                print("Warning: Could not add the track \"" + track.location + "\" as relative path to the playlist \"" + playlistName + "\"; added the track as absolute path instead.")
+                                                playlistContent += track.location + "\n"
+                                                # print("Warning: Could not add the track \"" + track.location_escaped + "\" as relative path to the playlist \"" + playlistName + "\"; added the track as absolute path instead.")
+                                                # playlistContent += track.location_escaped + "\n"
 
                 playlistPath = parentPath.joinpath(cleanupPlaylistName(playlist.name) + ".m3u")
                 playlistPath.write_text(playlistContent, encoding="utf8")
--- libpytunes/Library.py
+++ libpytunes/Library.py
@@ -25,7 +25,9 @@ class Library:
         self.musicPathXML = musicPathXML
         self.musicPathSystem = musicPathSystem
         self.filesOnly = filesOnly
-        self.il = plistlib.readPlist(itunesxml)  # Much better support of xml special characters
+        #self.il = plistlib.readPlist(itunesxml)  # Much better support of xml special characters
+        with open(itunesxml, 'rb') as fp :
+            self.il = plistlib.loads(fp.read())
         self.songs = {}
         self.getSongs()
 
